a1a9d6edde2cf5226ee94b2e3b77fdf3ce57f782
Aryan <ariand2083@gmail.com>
2025-10-23 05:47:37 -0700

    add UserTypeEnum

---
+++ Health.Domain/Enum/UserEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Health.Domain.Enum
+{
+	internal class UserEnum
+	{
+		
+		public enum UserTypeEnum
+		{
+			[Display(Name = "دکتر")]
+			Doctor=1,
+			[Display(Name = "بیمار")]
+			Patient =2
+		}
+	}
+}
