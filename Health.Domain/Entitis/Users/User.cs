80744cfd158af4b2aa16217d0c29de9c11fc9d68
Kiana <Kianafti.znd@gmail.com>
2025-10-23 16:43:28 +0330

    --UpdateBsaeEntity&&User

--- Health.Domain/Entitis/Users/User.cs
+++ Health.Domain/Entitis/Users/User.cs
@@ -8,13 +8,20 @@ namespace Health.Domain.Entitis.Users
     {
         
         public string NationalId { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
-        public string Email { get; set; }
+
+        public string? Email { get; set; }
+
         public string PhoneNumber { get; set; }
+
         public string PasswordHash { get; set; }
-        public string UserName { get; set; }
-        public string Bio { get; set; }
+
+        public string? UserName { get; set; }
+
+        public string? Bio { get; set; }
         /// <summary>
         /// we must have class for them later
         ////so they are some kide of class and reprasenting for
