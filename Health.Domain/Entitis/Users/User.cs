e057c949a5b30e5bcd2612897f3a83c20d708734
Aryan <ariand2083@gmail.com>
2025-10-30 07:39:25 -0700

    edit user

--- Health.Domain/Entitis/Users/User.cs
+++ Health.Domain/Entitis/Users/User.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Health.Domain.Base;
 using Health.Domain.Enum;
 
 namespace Health.Domain.Entitis.Users
 {
+	[DisplayName("کاربران")]
+	[Table(name: "User", Schema = "Health")]
     public class User:BaseEntity
     {
 		#region Propertis
-		[Display(Name = nameof(Resources.Resource.NationalId), ResourceType = typeof(Resources.Resource))]
-		public string NationalId { get; set; }
+		//[Display(Name = nameof(Resources.Resource.NationalId), ResourceType = typeof(Resources.Resource))]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Code { get; set; }
+		public string NationalCode { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
@@ -16,6 +22,7 @@ namespace Health.Domain.Entitis.Users
 		public string PasswordHash { get; set; }
 		public string UserName { get; set; }
 		public string Bio { get; set; }
+		[Required]
 		public UserEnum.UserTypeEnum UserType { get; set; }
 
 		#endregion
